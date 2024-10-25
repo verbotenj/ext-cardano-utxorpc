9091ad962acf58f955ea43012315c50921dd11d4
Paulo Bressan <paulo.bressan@outlook.com>
2024-10-25 16:40:43 -0300

    feat: added crdgen json output (#13)

--- operator/src/crdgen.rs
+++ operator/src/crdgen.rs
@@ -1,8 +1,17 @@
 use kube::CustomResourceExt;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 && args[1] == "json" {
+        print!(
+            "{}",
+            serde_json::to_string_pretty(&operator::UtxoRpcPort::crd()).unwrap()
+        );
+        return;
+    }
+
     print!(
         "{}",
-        serde_yaml::to_string(&operator::UtxoRpcPort::crd()).unwrap()
+        serde_json::to_string(&operator::UtxoRpcPort::crd()).unwrap()
     )
 }
